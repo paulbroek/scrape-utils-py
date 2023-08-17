9905d78ab0ec54fdcb7fde1be24840ca507d3c5b
Paul Broek <pauliusbroek@hotmail.com>
2023-08-17 23:30:46 +0200

    add settings BaseModel

--- scrape_utils/core/config.py
+++ scrape_utils/core/config.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+# pydantic v2
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseSettings
+
 
 class UppercaseStrEnum(Enum):
     def __init__(self, value):
@@ -10,3 +14,19 @@ class devEnvs(UppercaseStrEnum):
     DEV = "DEV"
     TEST = "TEST"
     PROD = "PROD"
+
+
+class MyBaseSettings(BaseSettings):
+    # Base
+    api_v1_prefix: str
+    debug: bool
+    project_name: str
+    version: str
+    description: str
+    data_dir: str
+
+    # model_config = SettingsConfigDict(env_file=".env")
+
+    # neccesary to support lru_cache in fastapi dependency injection
+    def __hash__(self):
+        return hash((type(self),) + tuple(self.__dict__.values()))
